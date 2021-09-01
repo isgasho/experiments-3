b2d2c90dd7c5e8ee43007d155717ec81027e7d54
Gyuho Lee <leegyuho@amazon.com>
2021-09-01 14:53:40 -0700

    monitor-rust: add main
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- monitor-rust/src/main.rs
+++ monitor-rust/src/main.rs
@@ -1,3 +1,23 @@
+#![warn(clippy::all)]
+
+use std::time::Duration;
+
+mod server;
+
 fn main() {
-    println!("Hello, world!");
+    // fix flags... clap has some dependency issue with tokio
+    let srv = server::Handler::new(
+        8001,
+        Duration::from_secs(10),
+        String::from("wss://observer.terra.dev"),
+    );
+
+    let future_task = srv.start();
+    let rt = tokio::runtime::Runtime::new().unwrap();
+
+    let ret = rt.block_on(future_task);
+    match ret {
+        Ok(_) => println!("server done"),
+        Err(e) => println!("server aborted {}", e),
+    };
 }
