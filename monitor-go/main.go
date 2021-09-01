bc677a17d11670ea22117bd7ff5e7c910f250dcb
Gyuho Lee <leegyuho@amazon.com>
2021-09-01 14:41:17 -0700

    monitr-go: add CLI
    Signed-off-by: Gyuho Lee <leegyuho@amazon.com>

--- monitor-go/main.go
+++ monitor-go/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"time"
+)
 
 func main() {
-	fmt.Println("hello")
+	listenerPort := flag.Uint64("listener-port", 8080, "listener port")
+	requestTimeoutSeconds := flag.Uint64("request-timeout-seconds", 10, "request timeout in seconds")
+	observerURL := flag.String("observer-url", "wss://observer.terra.dev", "observer URL")
+	flag.Parse()
+
+	srv := newHandler(*listenerPort, time.Duration(*requestTimeoutSeconds)*time.Second, *observerURL)
+	srv.start()
 }
